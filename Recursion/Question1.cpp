2fa306a5e69134dea158a2008ee6caf83743407a
unknown <chaudharyujjwal302@gmail.com>
2021-08-03 13:50:29 +0530

    Recursion

---
+++ Recursion/Question1.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+
+using namespace std;
+
+void countinghead(int n){
+    if(n==0){
+        return;
+    }
+    cout<<n<<endl;
+    countinghead(n-1);
+    
+}
+void countingtail(int n){
+    if(n==0){
+        return;
+    }
+    countingtail(n-1);
+    cout<<n<<endl;
+}
+int factorial(int n){
+    if(n==1){
+        return 1;
+    }
+    return (n*factorial(n-1));
+}
+int fibo(int n){
+    if(n==0 || n==1){
+        return n;
+    }
+    return fibo(n-1)+fibo(n-2); 
+}
+
+void getdigit(int n){
+    if(n<=0){
+        return;
+    }
+    getdigit(n/10);
+    n=n%10;
+    cout<<n<<endl;
+}
+
+
+int main(){
+    cout<<"Enter a number: ";
+    int n,fact,fibos,s=6753;
+    cin>>n;
+    countinghead(n);
+    countingtail(n);
+    fact=factorial(n);
+    fibos=fibo(n);
+    //cout<<fact<<endl;
+    //cout<<fibos;
+    getdigit(s);
+    return 0;
+}
