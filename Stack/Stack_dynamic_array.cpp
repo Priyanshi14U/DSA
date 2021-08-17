b92697e8e67521a94a32a565c4fd91b56f0a02b3
unknown <chaudharyujjwal302@gmail.com>
2021-08-17 11:04:41 +0530

    dynamic stack

---
+++ Stack/Stack_dynamic_array.cpp
@@ -0,0 +1,144 @@
+#include<iostream> 
+ 
+using namespace std; 
+
+
+ 
+struct Stack{ 
+    int top; 
+    int capacity; 
+    int *array; 
+}; 
+struct Stack *createStack(int capacity){ 
+    struct Stack *S=(struct Stack*)malloc(sizeof(struct Stack)); 
+    if(!S){ 
+        return NULL; 
+    } 
+    S->capacity=capacity; 
+    S->top=-1; 
+    S->array=(int *)malloc(S->capacity*sizeof(int)); 
+    if(!S->array){ 
+        return NULL; 
+    } 
+    return S; 
+} 
+ 
+int isFull(struct Stack *S){ 
+    return (S->top==S->capacity-1); 
+} 
+
+int isEmpty(struct Stack *S){
+    return (S->top==-1);
+}
+
+
+void doubleStack(struct Stack *S){
+    S->capacity*=2;
+    S->array=(int *)realloc(S->array,S->capacity*sizeof(int));
+    cout<<"Memory increased"<<endl;
+}
+ 
+void push(struct Stack *S,int data){
+    if(!S){
+        cout<<"Stack already deleted";
+        return;
+    }
+    else{
+        if(isFull(S)){ 
+        doubleStack(S);
+        } 
+        S->top++; 
+        S->array[S->top]=data; 
+        cout<<data<<" pushed!"<<endl; 
+        return;
+    }
+     
+} 
+
+void halfStack(struct Stack *S){
+    S->capacity/=2;
+    S->array=(int *)realloc(S->array,S->capacity*sizeof(int));
+    cout<<"Memory decreased"<<endl;
+}
+ 
+int pop(struct Stack *S){
+    if(!S){
+        cout<<"Stack already deleted";
+        return -1;
+    }
+    else{
+        if(isEmpty(S)){
+        cout<<"Underflow"<<endl;
+        return -1;
+        }
+        cout<<"Element deleted : "<<S->array[S->top]<<endl;
+        S->top--;
+        if(S->top==(S->capacity-1)/2){
+            halfStack(S);
+        }
+        return S->array[S->top];
+    }
+    
+}
+
+int peek(struct Stack *S)
+{
+    if(!S){
+        cout<<"Stack already deleted";
+        return -1;
+    }
+    else{
+        if(isEmpty(S)){
+            return -1;
+        }
+        return S->array[S->top];
+    }
+    
+}
+void deleteStack(struct Stack *S){
+    if(S){
+        if(S->array){
+            free(S->array);
+        }
+        free(S);
+        cout<<"Stack Deleted "<<endl;
+    }
+    return;
+}
+
+
+int main(){ 
+    int choice=1,capacity=1,capacity2=20,n,pet; 
+    struct Stack *stk=createStack(capacity); 
+    
+    
+    while(choice!=0){
+        cout<<"Enter your choice: "; 
+        cin>>choice;
+        switch(choice){ 
+        case 0:
+            exit(1);
+            break;
+        case 1: 
+            cout<<"Enter your number : ";
+            cin>>pet;
+            push(stk,pet);
+            break; 
+        case 2: 
+            pop(stk);
+            break;
+        case 3:
+            n=peek(stk);
+            cout<<"Element at top : "<<n<<endl;
+            break; 
+        case 4:
+            deleteStack(stk);
+            break;
+        default: 
+        cout<<"Enter valid choice"<<endl; 
+        break; 
+        } 
+    }
+    
+    return 0; 
+}
